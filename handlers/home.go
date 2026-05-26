04009a2778fd00257050fb93ee43a7354ed80e06
Wilfrid <wilfrid.delamare@ynov.com>
2026-05-26 22:54:40 +0200

    modernRoutes2

--- handlers/home.go
+++ handlers/home.go
@@ -21,11 +21,6 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, data any) {
 }
 
 func Home( w http.ResponseWriter, r *http.Request) {
-	// interdire les methodes non necessaires
-	if r.Method != http.MethodGet {
-		http.Error(w, "Methode interdite", http.StatusMethodNotAllowed)
-		return
-	}
 	data := fake.GetAllPosts()
 	RenderTemplate(w, "index.tmpl", data)
 }
