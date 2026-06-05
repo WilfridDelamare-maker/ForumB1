69740d8f18b6740028860ca7e94571ae03f9b48a
Wilfrid <wilfrid.delamare@ynov.com>
2026-06-05 21:03:20 +0200

    uploadImg

--- handlers/postCreator.go
+++ handlers/postCreator.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"forum/fake"
 	"forum/models"
+	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -48,6 +50,33 @@ func PostCreator(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/posts/create?error=1", http.StatusSeeOther)
 		return
 	}
+
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		fmt.Println("aucune image uploadée askip")
+	} else {
+		defer file.Close()
+
+		fmt.Println("file name:", header.Filename)
+		fmt.Println("file size:", header.Size)
+
+		imgPath := "./static/upload/" + header.Filename
+
+		destination, err := os.Create(imgPath)
+		if err != nil {
+			http.Error(w, "Erreur lors de la sauvegarde image", http.StatusInternalServerError)
+			return
+		}
+		defer destination.Close()
+
+		_, err = io.Copy(destination, file)
+		if err != nil {
+			http.Error(w, "Erreur de copie de l'image", http.StatusInternalServerError)
+			return
+		}
+		fmt.Println("Image sauvegardée au chemin:", imgPath)
+	}
+
 
 	fmt.Println("title:", title, "content:", content, "categories:")
 	for _, category := range categories {
